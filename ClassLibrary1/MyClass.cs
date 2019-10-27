4c4e38b62cc5f9745fa0aa222fa945a97df4265b
eliezergensburger <eliezer.jct@gmail.com>
2019-10-27 13:33:30 +0200

    zehu

---
+++ ClassLibrary1/MyClass.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassLibrary1
+{
+    class MyClass
+    {
+        private int m_x;
+
+        public int X
+        {
+            get { return m_x; }
+            set { m_x = value; }
+        }
+        public override string ToString()
+        {
+            return X.ToString();
+        }
+    }
+}
---
+++ ClassLibrary1/Product.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassLibrary1
+{
+    public class Product
+    {
+        public int ID { get; private set; }
+        public String Name { get; set; }
+
+        private static int code = 0;
+
+        public Product(string name)
+        {
+            Name = name;
+            ID = code++;
+        }
+
+        public static string getCode()
+        {
+            return String.Format("the current code is {0} :",code);
+        }
+    }
+}
